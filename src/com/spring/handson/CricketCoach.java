439c1964bdd2a705f07806a096a72b5cf9495dd5
chamikab <kasun.chamika@gmail.com>
2018-11-04 18:53:14 +0530

    Added changes for reading property values from properties file to Spring config file.

--- src/com/spring/handson/CricketCoach.java
+++ src/com/spring/handson/CricketCoach.java
@@ -2,6 +2,8 @@ package com.spring.handson;
 
 public class CricketCoach implements Coach {
     private FourtuneService fortuneService;
+    private String emailAddress;
+    private String team;
 
     public CricketCoach() {
         System.out.println("Hey this is Cricket Coach No Arg Constructor.");
@@ -20,5 +22,23 @@ public class CricketCoach implements Coach {
     @Override
     public String getDailyFortune() {
         return "This is Cricket Daily Fortune " + fortuneService.getFortune();
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("Hello!! Cricket coach setting up email.");
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("Hello!! Cricket coach setting up team");
+        this.team = team;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
     }
 }
--- src/com/spring/handson/HelloSpringApp.java
+++ src/com/spring/handson/HelloSpringApp.java
@@ -5,15 +5,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class HelloSpringApp {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/spring/handson/applicationContext.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        // Constructor Dependency Injection.
         Coach coach = context.getBean("myCoach", Coach.class);
         System.out.println(coach.getDailyWorkout());
         System.out.println(coach.getDailyFortune());
 
-        Coach cricketCoach = context.getBean("myCricketCoach", Coach.class);
+        // Method setter Dependency Injection.
+        CricketCoach cricketCoach = context.getBean("myCricketCoach", CricketCoach.class);
         System.out.println(cricketCoach.getDailyWorkout());
         System.out.println(cricketCoach.getDailyFortune());
+        System.out.println(cricketCoach.getEmailAddress());
+        System.out.println(cricketCoach.getTeam());
 
         context.close();
     }
